41905196eab50913b0cd5bfbcfb9b5e66ff8f0bd
Suzaev Amin <72074078+King-Codde@users.noreply.github.com>
2021-06-04 21:00:25 +0300

    Fixed

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -4,42 +4,6 @@
 #include <cstring>
 const int n = 10;
 
-//Монстры
-enum ENEMIES
-{
-    ENEMIES_OGR_hp = 15,
-    ENEMIES_OGR_dmg = 6,
-    ENEMIES_OGR_exp = 6,
-    ENEMIES_OGR_money = 14,
-    ENEMIES_ELF_hp = 8,
-    ENEMIES_ELF_dmg = 12,
-    ENEMIES_ELF_exp = 8,
-    ENEMIES_ELF_money = 17,
-    ENEMIES_WOOD_GOBLIN_hp = 18,
-    ENEMIES_WOOD_GOBLIN_dmg = 8,
-    ENEMIES_WOOD_GOBLIN_exp = 7,
-    ENEMIES_WOOD_GOBLIN_money = 20,
-    ENEMIES_SLIZEN_hp = 10,
-    ENEMIES_SLIZEN_dmg = 4,
-    ENEMIES_SLIZEN_exp = 4,
-    ENEMIES_SLIZEN_money = 10,
-    ENEMIES_WILD_DOG_hp = 5,
-    ENEMIES_WILD_DOG_dmg = 4,
-    ENEMIES_WILD_DOG_exp = 2,
-    ENEMIES_WILD_DOG_money = 5,
-};
-//Оружие и Броня
-enum WEAPON_AND_ARMOR
-{
-    WEAPON_money_IRON_CLUB = 40,
-    WEAPON_dmg_IRON_CLUB = 5,
-    WEAPON_money_STEEL_SWORD = 150,
-    WEAPON_dmg_STEEL_SWORD = 15,
-    ARMOR_money_LEATHER_ARMOR = 70,
-    ARMOR_hp_LEATHER_ARMOR = 50,
-    ARMOR_money_IRON_ARMOR = 300,
-    ARMOR_hp_IRON_ARMOR = 250,
-};
 //Команды для Кузни
 enum SMITH_COMMAND
 {
@@ -48,46 +12,47 @@ enum SMITH_COMMAND
     SMITH_COMMAND_LEATHER_ARMOR = 3,
     SMITH_COMMAND_IRON_ARMOR = 4,
 };
+
 class EnemyAndCo
 {
 public:
-    int enemy_ogr_hp = ENEMIES_OGR_hp;
-    int enemy_ogr_dmg = ENEMIES_OGR_dmg;
-    int enemy_ogr_exp = ENEMIES_OGR_exp;
-    int enemy_ogr_money = ENEMIES_OGR_money;
+    int enemy_ogr_hp = 15;
+    int enemy_ogr_dmg = 6;
+    int enemy_ogr_exp = 6;
+    int enemy_ogr_money = 14;
 
-    int enemy_elf_hp = ENEMIES_ELF_hp;
-    int enemy_elf_dmg = ENEMIES_ELF_dmg;
-    int enemy_elf_exp = ENEMIES_ELF_exp;
-    int enemy_elf_money = ENEMIES_ELF_money;
+    int enemy_elf_hp = 8;
+    int enemy_elf_dmg = 12;
+    int enemy_elf_exp = 8;
+    int enemy_elf_money = 17;
 
-    int enemy_wood_hp = ENEMIES_WOOD_GOBLIN_hp;
-    int enemy_wood_dmg = ENEMIES_WOOD_GOBLIN_dmg;
-    int enemy_wood_exp = ENEMIES_WOOD_GOBLIN_exp;
-    int enemy_wood_money = ENEMIES_WOOD_GOBLIN_money;
+    int enemy_wood_hp = 18;
+    int enemy_wood_dmg = 8;
+    int enemy_wood_exp = 7;
+    int enemy_wood_money = 20;
 
-    int enemy_slizen_hp = ENEMIES_SLIZEN_hp;
-    int enemy_slizen_dmg = ENEMIES_SLIZEN_dmg;
-    int enemy_slizen_exp = ENEMIES_SLIZEN_exp;
-    int enemy_slizen_money = ENEMIES_SLIZEN_money;
+    int enemy_slizen_hp = 10;
+    int enemy_slizen_dmg = 4;
+    int enemy_slizen_exp = 4;
+    int enemy_slizen_money = 10;
 
-    int enemy_wild_dog_hp = ENEMIES_WILD_DOG_hp;
-    int enemy_wild_dog_dmg = ENEMIES_WILD_DOG_dmg;
-    int enemy_wild_dog_exp = ENEMIES_WILD_DOG_exp;
-    int enemy_wild_dog_money = ENEMIES_WILD_DOG_money;
+    int enemy_wild_dog_hp = 5;
+    int enemy_wild_dog_dmg = 4;
+    int enemy_wild_dog_exp = 2;
+    int enemy_wild_dog_money = 5;
 
 };
 class wepon_and_armor
 {
 public:
-    int weapon_money_iron_club = WEAPON_money_IRON_CLUB;
-    int weapon_dmg_iron_club = WEAPON_dmg_IRON_CLUB;
-    int weapon_money_steel_sword = WEAPON_money_STEEL_SWORD;
-    int weapon_dmg_steel_sword = WEAPON_dmg_STEEL_SWORD;
-    int armor_money_leather_armor = ARMOR_money_LEATHER_ARMOR;
-    int armor_hp_leather_armor = ARMOR_hp_LEATHER_ARMOR;
-    int armor_money_iron_armor = ARMOR_money_IRON_ARMOR;
-    int armor_hp_iron_armor = ARMOR_hp_IRON_ARMOR;
+    int weapon_money_iron_club = 40;
+    int weapon_dmg_iron_club = 5;
+    int weapon_money_steel_sword = 150;
+    int weapon_dmg_steel_sword = 15;
+    int armor_money_leather_armor = 70;
+    int armor_hp_leather_armor = 50;
+    int armor_money_iron_armor = 300;
+    int armor_hp_iron_armor = 250;
 };
 
 
